e3ef7eb12a65fb7a19ef56e54c451ce85505fd74
Aman <arl0817osho@gmail.com>
2024-07-10 03:07:44 +0530

    Update route for Hello controller to "/hello"

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func main() {
 	r.POST("/signup", controllers.SignUp)
 	r.POST("/login", controllers.Login)
 	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
-	r.GET("/", controllers.Hello)
+	r.GET("/hello", controllers.Hello)
 	r.Run()
 }
