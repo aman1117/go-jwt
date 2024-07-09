bd71aefbe53a0ec005fce6573d2d5beaf23e17af
Aman <arl0817osho@gmail.com>
2024-07-10 03:04:14 +0530

    chore: Add print statement to Hello controller

--- controllers/hello.controller.go
+++ controllers/hello.controller.go
@@ -1,8 +1,13 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Hello(c *gin.Context) {
+	fmt.Print("Hello, World!")
 	c.JSON(200, gin.H{
 		"message": "Hello, World!",
 	})
